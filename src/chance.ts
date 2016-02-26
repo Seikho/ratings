70b2f365c45a4cacb20485a8eed0cce04e00a7da
Carl Winkler <carl@carlossus.com>
2016-02-26 23:06:28 +0800

    Rename api functions

--- src/chance.ts
+++ src/chance.ts
@@ -14,13 +14,14 @@ const CHANCE_TABLE = [
     { difference: 799, chance: 0.99 },
 ]
 
-// TODO: Make configurable
+
 const K_FACTOR = 10;
 const DEFAULT_SCORE_LIMIT = 1;
+
 /**
  * @returns The chance for the highest rated player to win (0.0 -> 1.0)
  */
-export function winChance(homeRating: number, awayRating: number): number {
+export function chance(homeRating: number, awayRating: number): number {
     let difference = Math.abs(homeRating - awayRating);
     let chanceRow = CHANCE_TABLE.filter(c => difference <= c.difference)[0];
     
@@ -29,20 +30,20 @@ export function winChance(homeRating: number, awayRating: number): number {
         : 1;    
 }
 
-export function expectedResult(homeRating: number, awayRating: number, options?: Options): number {
+export function expected(homeRating: number, awayRating: number, options?: Options): number {
     options = options || {};
     let scoreUpperLimit = options.scoreUpperLimit || DEFAULT_SCORE_LIMIT;
     
-    let chance = winChance(homeRating, awayRating);
+    let winChance = chance(homeRating, awayRating);
     
-    return ((scoreUpperLimit * 2) * chance - scoreUpperLimit) * (homeRating > awayRating ? 1 : -1) 
+    return ((scoreUpperLimit * 2) * winChance - scoreUpperLimit) * (homeRating > awayRating ? 1 : -1) 
 }
 
 export function adjustment(homeRating: number, awayRating: number, result: number, options?: Options): { home: number, away: number } {
     options = options || {};
     let kFactor = options.kFactor || K_FACTOR;
-    let expected = expectedResult(homeRating, awayRating);
-    let ratingChange = Math.abs(K_FACTOR * (result - expected));
+    let expectedResult = expected(homeRating, awayRating);
+    let ratingChange = Math.abs(K_FACTOR * (result - expectedResult));
     
     let isHomeWinner = result > 0;
     let isAwayWinner = result < 0;
